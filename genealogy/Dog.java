2da5613745dfdc2a6474ba4a58532d840fe72c23
gatlinglolx <shahov199@gmail.com>
2023-12-12 10:24:38 +0300

    Dog_1

---
+++ genealogy/Dog.java
@@ -0,0 +1,21 @@
+package genealogy;
+
+import java.util.*;
+
+public class Dog {
+    private String name;
+    private Date birthDate;
+
+    public Dog(String name, Date birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+}
